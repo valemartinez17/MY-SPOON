645dde1b450aa7211fce9fddba36ecf47bed3625
valemartinez17 <valeria17.m@gmail.com>
2021-03-23 09:48:04 -0400

    Created File

---
+++ MY-SPOON/MY-SPOON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MY-SPOON
+//
+//  Created by Valeria Martinez Brenes on 3/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MY-SPOON/MY-SPOON/MY_SPOONApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_SPOONApp.swift
+//  MY-SPOON
+//
+//  Created by Valeria Martinez Brenes on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MY_SPOONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
